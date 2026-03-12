dae5a35caaf7265cac746ba44050e601a3a5d093
GarGrigoryan <garikyang950@gmail.com>
2026-03-12 20:55:13 +0400

    fetched wind data for 12.03.2026 20:53 in Armenia 42 cities

--- arm_fetcher.py
+++ arm_fetcher.py
@@ -1,48 +1,66 @@
 import requests
 import csv
 import os
+from dotenv import load_dotenv
 
+# load API key
+load_dotenv()
 API_KEY = os.getenv("API_KEY")
 
-cities = [
-    ("Yerevan", 44.4991, 40.1792),
-    ("Gyumri", 43.8475, 40.7894),
-    ("Vanadzor", 44.4939, 40.8128),
-    ("Hrazdan", 44.7667, 40.5000),
-    ("Abovyan", 44.6333, 40.2667),
-    ("Kapan", 46.4167, 39.2000),
-    ("Armavir", 44.0500, 40.1500),
-    ("Artashat", 44.5500, 39.9667),
-    ("Gavar", 45.1333, 40.3500)
-]
+input_file = "armenia_cities.csv"
+output_file = "armenia_wind.csv"
 
 url = "https://api.openweathermap.org/data/2.5/weather"
 
 rows = []
 
-for city, lon, lat in cities:
-    params = {
-        "lat": lat,
-        "lon": lon,
-        "appid": API_KEY,
-        "units": "metric"
-    }
+with open(input_file, encoding="utf-8") as f:
+    reader = csv.DictReader(f, delimiter=";")
 
-    response = requests.get(url, params=params)
-    data = response.json()
+    for row in reader:
+        city = row["city"]
+        lon = float(row["longitude"])
+        lat = float(row["latitude"])
 
-    if "wind" in data:
-        wind_speed = int(data["wind"]["speed"])
-        wind_dir = int(data["wind"]["deg"])
-    else:
-        wind_speed = -1
-        wind_dir = -1
+        print(f"Fetching wind data for {city}...")
 
-    rows.append([city, lon, lat, wind_speed, wind_dir])
+        params = {
+            "lat": lat,
+            "lon": lon,
+            "appid": API_KEY,
+            "units": "metric"
+        }
 
-with open("armenia_wind.csv", "w", newline="") as f:
-    writer = csv.writer(f)
-    writer.writerow(["city", "longitude", "latitude", "wind_speed", "wind_direction"])
+        try:
+            response = requests.get(url, params=params)
+            data = response.json()
+
+            if "wind" in data:
+                wind_speed = int(data["wind"]["speed"])
+                wind_dir = int(data["wind"]["deg"])
+            else:
+                wind_speed = -1
+                wind_dir = -1
+
+        except Exception as e:
+            print("Error:", e)
+            wind_speed = -1
+            wind_dir = -1
+
+        rows.append([city, lon, lat, wind_speed, wind_dir])
+
+# write CSV with ; separator
+with open(output_file, "w", newline="", encoding="utf-8") as f:
+    writer = csv.writer(f, delimiter=";")
+
+    writer.writerow([
+        "city",
+        "longitude",
+        "latitude",
+        "wind_speed",
+        "wind_direction"
+    ])
+
     writer.writerows(rows)
 
-print("CSV file created: armenia_wind.csv")
+print("Finished. File saved as armenia_wind.csv")
